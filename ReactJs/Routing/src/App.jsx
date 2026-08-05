cb6eca1ac34173eeebba871ac3542f098f501002
Tayyab765 <attiqtayyab.1003@gmail.com>
2026-08-05 15:38:47 +0500

    Learned Protected Route

--- ReactJs/Routing/src/App.jsx
+++ ReactJs/Routing/src/App.jsx
@@ -9,8 +9,12 @@ import { Profile } from "./pages/Profile";
 import DashboardHome from "./pages/DashboardHome";
 import { Products } from "./pages/Products";
 import { ProductView } from "./pages/ProductView";
+import { Login } from "./pages/Login";
+import { ProtectedRoute } from "./pages/ProtectedRoute";
 
 function App() {
+  const loginStatus = false;
+
   return (
     <>
       <BrowserRouter>
@@ -20,8 +24,21 @@ function App() {
           <Route path="/contact" element={<Contact />} />
           <Route path="/products" element={<Products />}></Route>
           <Route path="/product_view/:id" element={<ProductView />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          {/* <Route
+            path="/protected"
+            element={<ProtectedRoute loggedIn={loginStatus} />}
+          ></Route> */}
 
-          <Route path="/dashboard" element={<Dashboard />}>
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute loggedIn={loginStatus}>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+            c
+          >
             <Route index element={<DashboardHome />}></Route>
             <Route path="profile" element={<Profile />}></Route>
             <Route path="settings" element={<Settings />}></Route>
