0c7d253e445c541a133425d92957fc34d2653c80
Tayyab765 <attiqtayyab.1003@gmail.com>
2026-08-18 14:18:26 +0500

    Learned POST request in Node using req.on method

--- NodeJs/Backend/Second/App.js
+++ NodeJs/Backend/Second/App.js
@@ -3,6 +3,27 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const users = {
+  user1: {
+    name: "Tayyab Attiq",
+    email: "tayyab@example.com",
+    job: "Software Engineer",
+    age: 22,
+  },
+  user2: {
+    name: "Ayesha Khan",
+    email: "ayesha@example.com",
+    job: "Data Scientist",
+    age: 25,
+  },
+  user3: {
+    name: "Zain Malik",
+    email: "zain@example.com",
+    job: "UI/UX Designer",
+    age: 28,
+  },
+};
+
 const myserver = http.createServer((req, res) => {
   console.log(req.method);
 
@@ -14,7 +35,7 @@ const myserver = http.createServer((req, res) => {
     switch (req.method) {
       case "GET":
         res.statusCode = 200;
-        return res.end("Users URL triggered using GET method");
+        return res.end(JSON.stringify(users));
 
       case "PUT":
         res.statusCode = 204;
@@ -24,10 +45,30 @@ const myserver = http.createServer((req, res) => {
         res.statusCode = 204;
         return res.end("Users URL triggered using DELETE method");
 
-      case "POST":
-        res.statusCode = 201;
-        return res.end("Users URL triggered using POST method");
+      case "POST": {
+        let tempUser = "";
+        req.on("data", (chunk) => {
+          tempUser += chunk;
+        });
+        req.on("end", () => {
+          const parsedData = JSON.parse(tempUser);
+          if (typeof parsedData === "string") {
+            parsedData = JSON.parse(parsedData);
+          }
 
+          let userkey = `user4`;
+          console.log("Parsed Incoming Payload:", parsedData);
+          users[userkey] = {
+            name: parsedData.name || "Unknown",
+            email: parsedData.email || "No Email",
+            job: parsedData.job || "Unemployed",
+            age: parsedData.age ? Number(parsedData.age) : 0,
+          };
+
+          return res.end(JSON.stringify(users));
+        });
+        return;
+      }
       default:
         res.statusCode = 405;
         return res.end("Unknown method found");
